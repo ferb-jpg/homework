2f15d796fd9b0060596e8510f09f8576f531ff35
ferb-jpg <palmera_luisa1290@americana.edu.co>
2026-08-26 23:54:55 -0500

    Update situacion_estudiante.py

---
+++ situacion_estudiante.py
@@ -0,0 +1,38 @@
+nombre = input("Ingrese su primer y segundo nombre: ")
+apellido = input("Ingrese su primer y segundo apellido: ")
+edad = int(input("Ingrese su edad: "))
+ciudad = input("ingrese su ciudad: ")
+universidad = input("Ingrese la universidad donde estudia: ")
+carrera = input("Ingrese la carrera que estudia: ")
+semestre = int(input("Ingrese su semestre actual: "))
+promedio = float(input("Ingrese su promedio: "))
+
+nombre_completo = nombre + " " + apellido
+
+print("==== Perfil del estudiante ====")
+print("Nombre completo: ", nombre_completo)
+print("Edad: ", edad)
+print("Ciudad: ", ciudad)
+print("Universidad: ", universidad)
+print("Carrera: ", carrera)
+print("Semestre: ", semestre)
+print("promedio: ", promedio)
+print("===============================")
+
+#Variables tipo str
+#nombre
+#apellido
+#ciudad
+#universidad
+#carrera
+#nombre_completo
+
+#Variables de tipo int
+#edad
+#semestre
+
+#Variables de tipo float
+#Promedio
+
+#Variables bool
+#En este programa no se usa ninguna variable booleana
