d46a474c69c13224843a576f9b289d1f03362850
ferb-jpg <palmera_luisa1290@americana.edu.co>
2026-08-26 23:49:40 -0500

    Update ejercicio1.py

---
+++ ejercicio1.py
@@ -0,0 +1,11 @@
+a= "Nombre: carlos y ferb"
+b= "Edad: 19 y 18"
+c= "Semestre: segundo semestre"
+d= "Programa: Ingenieria de sistemas"
+e= "Promedio: 4.0"
+
+print(a)
+print(b)
+print(c)
+print(d)
+print(e)
