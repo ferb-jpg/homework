2d97fdcda85536a9e031230c2e22ef14c504adde
ferb-jpg <palmera_luisa1290@americana.edu.co>
2026-08-26 23:51:56 -0500

    Update ejercicio8.py

---
+++ ejercicio8.py
@@ -0,0 +1,10 @@
+nota1 = float(input("Ingrese su primera nota: "))
+nota2 = float(input("Ingrese su segunda nota: "))
+nota3 = float(input("Ingrese su tercera nota: "))
+
+promedio = (nota1 + nota2 + nota3) / 3
+
+print("Nota1: ", nota1)
+print("Nota2: ", nota2)
+print("Nota3: ", nota3)
+print("Promedio: ", round(promedio, 2))
