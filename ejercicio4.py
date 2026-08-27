7b06ece4100370daa338d8f50bbfcc600ef62228
ferb-jpg <palmera_luisa1290@americana.edu.co>
2026-08-26 23:50:51 -0500

    Update ejercicio4.py

---
+++ ejercicio4.py
@@ -0,0 +1,12 @@
+numero1 = float(input("Ingrese el primer número: "))
+numero2 = float(input("Ingrese el segundo número: "))
+
+suma = numero1 + numero2
+resta = numero1 - numero2
+mult = numero1 * numero2
+div = numero1 / numero2
+
+print("Suma:", suma)
+print("Resta: ", resta)
+print("Multiplicación: ", mult)
+print("División: ", div)
