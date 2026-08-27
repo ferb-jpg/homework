1925b2e4585db3796b466a56a30c371121338ac5
ferb-jpg <palmera_luisa1290@americana.edu.co>
2026-08-26 23:51:24 -0500

    Update ejercicio6.py

---
+++ ejercicio6.py
@@ -0,0 +1,6 @@
+nombre = input("Ingrese su nombre: ")
+an = int(input("Ingrese su año de nacimiento: "))
+ac = int(input("ingrese el año actual: "))
+edad = ac - an
+
+print(nombre, "tiene aproximadamente", edad, "años.")
