0f9c1c04f1ca2be0629121579386d9816b310e99
Krlznim <carlosww32254@gmail.com>
2026-08-27 00:15:39 -0500

    ejercicio10.py

---
+++ ejercicio10.py
@@ -0,0 +1,6 @@
+dolar = float(input("Cantidad de dólares: "))
+valor_dolar = float(input("Ingrese el valor del dólar hoy: "))
+
+conver = dolar * valor_dolar
+
+print("Total en pesos colombianos: ", conver)
